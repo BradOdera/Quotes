064683ccfd0dfb0918575b525cd5e142282866a3
BradOdera <bradodera@gmail.com>
2019-10-29 04:58:45 +0300

    Add intial code to music-quotes, quote, quote-form and upvote-button

---
+++ src/app/music-quotes/music-quotes.component.spec.ts
@@ -0,0 +1,25 @@
+ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MusicQuotesComponent } from './music-quotes.component';
+
+describe('MarvelQuotesComponent', () => {
+  let component: MusicQuotesComponent;
+  let fixture: ComponentFixture<MusicQuotesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MusicQuotesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MusicQuotesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/music-quotes/music-quotes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote} from '../quote';
+
+@Component({
+  selector: 'app-music-quotes',
+  templateUrl: './music-quotes.component.html',
+  styleUrls: ['./music-quotes.component.css']
+})
+export class MusicQuotesComponent implements OnInit {
+
+  @Input() quote: Quote;
+  @Output()isComplete = new EventEmitter<boolean>();
+  quotes: any;
+
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+  completeQuote(isComplete, index){
+    if(isComplete){
+      this.quotes.splice(index,1);
+    }
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
